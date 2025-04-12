b7509bbf854a2d24e611c7b05f98f6b41ef1f4a6
dotienluong <dotienluong1402@gmail.com>
2025-04-12 17:15:17 +0700

    Update: Done mini project chat bot popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ChatbotIcon from "./Component/ChatbotIcon";
 import ChatForm from "./Component/ChatForm";
 import ChatMessage from "./Component/ChatMessage";
 
 function App() {
   const [chatHistory, setChatHistory] = useState([]);
-  const genarateBotResponse = (history) => {
+  const [showChatbot, setShowChatbot] = useState(false);
+  const chatBodyRef = useRef();
+  const genarateBotResponse = async (history) => {
     console.log(history);
-  }
+    history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
+
+    //Update history:
+    const updateHistory = (text) => {
+      setChatHistory(prev => [...prev.filter(msg => msg.text !== "Thinking..."), { role: "model", text: text }]);
+    }
+
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: history })
+    }
+    try {
+      const response = await fetch(import.meta.env.VITE_API_URL, requestOptions);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error.message || "Error fetching response");
+      }
+      const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1').trim();
+      updateHistory(apiResponseText);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching response:", error);
+    }
+  };
+
+  useEffect(() => {
+    // auto scroll to bottom
+    chatBodyRef.current.scrollTo({ top: chatBodyRef.current.scrollHeight, behavior: "smooth" });
+  }, [chatHistory]);
 
   return (
-    <div className="container">
+    <div className={`container ${showChatbot ? "show_chatbot" : ""}`}>
+      <button onClick={() => setShowChatbot(prev => !prev)} id="chatbot_toggler">
+        <span className="material-symbols-rounded">
+          mode_comment
+        </span>
+        <span className="material-symbols-rounded">
+          close
+        </span>
+      </button>
+
       <div className="chatbot_popup">
         {/* Header */}
         <div className="chat_header">
@@ -18,15 +59,13 @@ function App() {
             <ChatbotIcon />
             <h2 className="logo_text">Chat bot</h2>
           </div>
-          <button>
-              <span className="material-symbols-rounded">
-                keyboard_arrow_down
-              </span>
-            </button>
+          <button onClick={() => setShowChatbot(prev => !prev)} className="material-symbols-rounded">
+            keyboard_arrow_down
+          </button>
         </div>
 
         {/* Body */}
-        <div className="chat_body">
+        <div ref={chatBodyRef} className="chat_body">
           <div className="message bot_message">
             <ChatbotIcon />
             <p className="message_text">
@@ -34,7 +73,7 @@ function App() {
             </p>
           </div>
           {/* Tu dong render chat:  */}
-          {chatHistory.map((chat, index)=>(
+          {chatHistory.map((chat, index) => (
             <ChatMessage key={index} chat={chat} />
           ))}
         </div>
